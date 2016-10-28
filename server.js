90cb0201f5a2f066e1ea6293c764fe01a6d2a065
dharmenn <dharmenn@thoughtworks.com>
2016-10-28 14:14:39 +0530

    added a jade file and js file to genrate bar chart

--- server.js
+++ server.js
@@ -9,8 +9,11 @@ app.set('view engine', 'jade');
 app.use(express.static('./views'));
 
 app.get('/', function (req, res) {
-
     res.render('randomNumLineChart')
+});
+
+app.get('/barChartWitDiv', function (req, res) {
+    res.render('barChartWitDiv')
 });
 
 http.createServer(app).listen(3000, function () {
