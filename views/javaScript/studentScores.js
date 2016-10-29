98fbfa3e160a313b3fdb4ee4717427d42ba57453
dharmenn <dharmenn@thoughtworks.com>
2016-10-29 12:34:26 +0530

    displaying subject names

--- views/javaScript/studentScores.js
+++ views/javaScript/studentScores.js
@@ -1,16 +1,16 @@
 var data = [
-    {name:'ramesh',subject:'maths',score:87},
-    {name:'suresh',subject:'maths',score:45},
-    {name:'pokemon',subject:'english',score:65},
-    {name:'mary',subject:'kannada',score:44},
-    {name:'riya',subject:'science',score:72},
-    {name:'katie',subject:'social studies',score:82},
-    {name:'katie',subject:'maths',score:98},
-    {name:'ramesh',subject:'bengali',score:25},
-    {name:'suresh',subject:'science',score:55},
-    {name:'riya',subject:'tamil',score:75},
-    {name:'pokemon',subject:'sports',score:95},
-    {name:'pokemon',subject:'social studies',score:32}
+    {name: 'ramesh', subject: 'maths', score: 87},
+    {name: 'suresh', subject: 'maths', score: 45},
+    {name: 'pokemon', subject: 'english', score: 65},
+    {name: 'mary', subject: 'kannada', score: 44},
+    {name: 'riya', subject: 'science', score: 72},
+    {name: 'katie', subject: 'social studies', score: 82},
+    {name: 'katie', subject: 'maths', score: 98},
+    {name: 'ramesh', subject: 'bengali', score: 25},
+    {name: 'suresh', subject: 'science', score: 55},
+    {name: 'riya', subject: 'tamil', score: 75},
+    {name: 'pokemon', subject: 'sports', score: 95},
+    {name: 'pokemon', subject: 'social studies', score: 32}
 ];
 
 
@@ -20,6 +20,7 @@ var colorScale = d3.scaleOrdinal(d3.schemeCategory10).domain([0, 100]);
 
 
 var chart = d3.selectAll('.chart');
+var legend = d3.selectAll('.legendsValue');
 
 function createBarChart(data) {
     var bars = chart.selectAll('div').data(data, function (d) {
@@ -29,22 +30,49 @@ function createBarChart(data) {
         .style('height', 20)
         .attr('class', 'scoresBar');
 
-    d3.selectAll('div.scoresBar')
+    d3.selectAll('.scoresBar')
         .style('width', function (d) {
             return xScale(d.score)
         })
         .style('background', function (d) {
-            return colorScale(d.score)
+            return colorScale(d.subject)
         })
         .text(function (d) {
-            return d.name +" "+d.score;
+            return d.name + " " + d.score;
         });
 
-    d3.selectAll('div.scoresBar').data(data,function(d){return d.name + d.subject})
+
+    d3.selectAll('.scoresBar').data(data, function (d) {
+        return d.name + d.subject
+    })
         .exit().remove();
+
 }
 
+
+function displaySubject(data){
+
+    var legends = legend.selectAll('div').data(data, function (d) {
+        return d.subject;
+    });
+
+    legends.enter().append('div')
+        .attr('class', 'subjectName')
+        .style('width', 80)
+        .style('background', function (d) {
+            return colorScale(d.subject)
+        })
+        .text(function (d) {
+            return d.subject;
+        });
+
+    d3.selectAll('.subjectName').data(data, function (d) {
+        return d.subject;
+    }).exit().remove();
+
+}
 $(window).ready(function () {
     createBarChart(data);
+    displaySubject(data);
 
 });
