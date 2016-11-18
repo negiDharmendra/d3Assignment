b65d51a9d250f7792fde60578fb88dabfee5804e
Dharmendra Singh Negi <dharmenn@thoughtworks.com>
2016-11-18 13:22:54 +0530

    Implemented exercise 10

---
+++ views/javaScript/exercise10.js
@@ -0,0 +1,63 @@
+const WIDTH = 800;
+const HEIGHT = 650;
+const MARGIN = 30;
+
+const INNER_WIDTH = WIDTH - (MARGIN * 2);
+const INNER_HEIGHT = HEIGHT - (MARGIN * 2);
+
+var coordinates = [0,1,2,3,4,5,6,7,8,9];
+
+var cordXScale = d3.scaleLinear().range([0, INNER_WIDTH]).domain([0, 10]);
+var cordYScale = d3.scaleLinear().range([INNER_HEIGHT, 0]).domain([0, 1]);
+
+function setupSvg() {
+    d3.select('.lineChart').append('svg')
+        .attr('width', WIDTH)
+        .attr('height', HEIGHT)
+        .append('g')
+        .attr('transform', 'translate(' + MARGIN + ',' + MARGIN + ')');
+}
+
+var line = d3.line().x(function (cord) {
+    return cordXScale(cord);
+}).y(function (cord) {
+    return cordYScale(((Math.sin(3 * cord) + 1) / 2));
+});
+
+
+function d3TickFormat(tick) {
+    return (tick / 10.0).toFixed(1);
+}
+
+var xAxis = d3.axisBottom(cordXScale).ticks(10)
+var yAxis = d3.axisLeft(cordYScale).ticks(10).tickFormat(d3TickFormat);
+
+function drawAxis() {
+    var xAxisG = d3.select('svg g').append('g').attr('transform', 'translate(0,' + INNER_HEIGHT + ')');
+    xAxisG.call(xAxis);
+    var yAxisG = d3.select('svg g').append('g').attr('transform', 'translate(0,0)');
+    yAxisG.call(yAxis);
+}
+
+function drawLineChart(line) {
+    var g = d3.select('svg g').append('g');
+    g.append('path')
+        .attr('d', line(coordinates))
+        .attr('class', 'line');
+}
+
+function drawCircle() {
+    var g = d3.select('svg > g').append('g');
+    g.selectAll('circle').data(coordinates).enter().append('circle')
+        .attr('cx', function (d) {
+            return cordXScale(d)
+        })
+        .attr('cy', function (d) {
+            return cordYScale((Math.sin(3 * d) + 1) / 2);
+        }).attr('r', 5);
+}
+
+setupSvg();
+drawAxis();
+drawLineChart(line);
+drawCircle();
