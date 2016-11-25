be122a76bce3f503caabe273b246c17fd0bf2f69
Dharmendra Singh Negi <dharmenn@thoughtworks.com>
2016-11-26 00:24:15 +0530

    creating separate subGraph for variance

--- public/javaScript/exercise18.js
+++ public/javaScript/exercise18.js
@@ -1,19 +1,22 @@
-const WIDTH = 800;
+const WIDTH = 1200;
 const HEIGHT = 600;
 const MARGIN = 30;
 
-const INNER_WIDTH = WIDTH - (MARGIN * 2);
+const INNER_WIDTH = WIDTH - (MARGIN * 4);
 const INNER_HEIGHT = HEIGHT - (MARGIN * 2);
 var numbers = [12, 22, 53, 64, 15, 69, 100, 22, 12, 60, 30, 40];
-var xScale = d3.scaleBand().range([0, INNER_WIDTH]).domain(numbers);
-var yScale = d3.scaleLinear().range([INNER_HEIGHT, 0]).domain([0, d3.sum(numbers, function (d) {
+
+var xScale = d3.scaleBand().range([0, INNER_WIDTH]).domain(numbers).padding(10);
+var sumOfNumbers = d3.sum(numbers, function (d) {
     return d
-})]);
+});
+
+var yScale = d3.scaleLinear().range([INNER_HEIGHT, 0]).domain([0, sumOfNumbers]);
 
 var colors = d3.scaleOrdinal(d3.schemeCategory20);
 
-function createButton() {
-    var allMethod = ['Min', 'Max', 'Extent', 'Sum', 'Mean', 'Median', 'Quantile', 'Variance', 'Deviation'];
+function createCheckBox() {
+    var allMethod = ['Min', 'Max', 'Extent', 'Sum', 'Mean', 'Median', 'Quantile', 'Variance', 'Deviation', 'Scan', 'Bisect', 'Bisector', 'Ascending', 'Descending'];
 
     allMethod.forEach(function (element) {
         var color = colors(element);
@@ -37,6 +40,10 @@ function createButton() {
     });
 }
 
+function resetCheckBox(){
+    $('.check-box input').attr('checked',false);
+}
+
 
 function setUpSvg() {
     document.querySelector('.numbers-input input').value = numbers.join(' ');
@@ -46,13 +53,13 @@ function setUpSvg() {
         .style('border', 'solid 1px lightgray')
         .append('g')
         .attr('class', 'main-group')
-        .attr('transform', 'translate(' + MARGIN + ',' + MARGIN + ')');
+        .attr('transform', 'translate(' + (MARGIN * 2) + ',' + MARGIN + ')');
     return svg;
 }
 
 function defineDomain(numbers) {
-    var sum = numbers.reduce(function (prev, val) {
-        return Number(prev) + Number(val);
+    var sum = d3.sum(numbers, function (num) {
+        return num;
     });
     xScale.domain(numbers.concat([sum]));
     yScale.domain([0, sum]);
@@ -62,10 +69,11 @@ function createAxis() {
     var xAxisG = d3.select('.main-group').append('g').attr('class', 'axis').attr('transform', 'translate(0,' + INNER_HEIGHT + ')');
     var yAxisG = d3.select('.main-group').append('g').attr('class', 'axis');
     var xAxis = d3.axisBottom(xScale);
-    var yAxis = d3.axisLeft(yScale);
+    var yAxis = d3.axisLeft(yScale).ticks(20).tickSizeInner(-INNER_WIDTH);
 
     xAxisG.call(xAxis);
     yAxisG.call(yAxis);
+    xAxisG.selectAll('text').remove();
 }
 
 function d3UsageArraysStatisticalMethods(data, text) {
@@ -76,6 +84,7 @@ function d3UsageArraysStatisticalMethods(data, text) {
         .append('rect')
         .attr('class', 'bar')
         .attr('width', 30)
+    group.selectAll('rect')
         .attr('height', function (d) {
             return INNER_HEIGHT - yScale(d);
         })
@@ -87,7 +96,7 @@ function d3UsageArraysStatisticalMethods(data, text) {
 }
 
 
-function drawLine(data, color,className) {
+function drawLine(data, color, className) {
     var line = d3.line()
         .x(function (d) {
             return d.x
@@ -96,18 +105,13 @@ function drawLine(data, color,className) {
             return yScale(d.y)
         });
 
-    var lineGroup = d3.select('.main-group').append('g').attr('class', 'lines');
+    var lineGroup = d3.select('.main-group').append('g').attr('class', 'lines').classed(className, true);
     lineGroup.append('path')
         .attr('d', line(data))
-        .attr('class',className)
         .attr('stroke', color)
         .attr('stroke-width', 2)
 }
 
-function quantile(numbers) {
-    var quantile = +document.querySelector('.quantile-input input').value;
-    return d3.quantile(numbers, quantile);
-}
 
 function getInputNumbers() {
     var numbers = document.querySelector('.numbers-input input').value.trim().split(/\s+/);
@@ -117,6 +121,87 @@ function getInputNumbers() {
     return numbers;
 }
 
+
+function drawVariance(numbers) {
+    var SUB_HEIGHT = 300;
+    var SUB_WIDTH = 500;
+    var MARGIN = 30;
+    var INNER_SUB_WIDTH = SUB_WIDTH - MARGIN * 2;
+    var INNER_SUB_HEIGHT = SUB_HEIGHT - MARGIN * 2;
+
+    var variance = d3.variance(numbers, function (d) {
+        return d
+    });
+    var xSubScale = d3.scaleBand().range([0, INNER_SUB_WIDTH]).domain(numbers);
+    var ySubScale = d3.scaleLinear().range([INNER_SUB_HEIGHT, 0]).domain([0, variance]);
+
+    var xSubAxis = d3.axisBottom(xSubScale);
+    var ySubAxis = d3.axisLeft(ySubScale).ticks(20);
+
+    var subGraph = d3.select('.main-group').append('g').attr('class', 'sub-graph');
+    subGraph.append('rect').attr('width', SUB_WIDTH).attr('transform', 'translate(' + (INNER_WIDTH - SUB_WIDTH - MARGIN) + ')').attr('height', SUB_HEIGHT).attr('fill', 'rgba(128, 128, 128, 0.34)');
+
+    var group = subGraph.append('g').attr('transform', 'translate(' + (INNER_WIDTH - SUB_WIDTH) + ',30)');
+
+    var xSubAxisG = group.append('g').attr('class', 'axis').attr('transform', 'translate(0,' + INNER_SUB_HEIGHT + ')');
+    var ySubAxisG = group.append('g').attr('class', 'axis');
+
+    xSubAxisG.call(xSubAxis).selectAll('text').remove();
+    ySubAxisG.call(ySubAxis)
+    group.selectAll('rect').data(numbers).enter()
+        .append('rect')
+        .attr('class', 'bar')
+        .attr('width', 10)
+    group.selectAll('rect')
+        .attr('height', function (d) {
+            return INNER_SUB_HEIGHT - ySubScale(d);
+        })
+        .attr('x', xSubScale)
+        .attr('y', ySubScale)
+        .attr('fill', 'steelblue');
+
+    group.selectAll('rect').exit().remove();
+
+
+    var line = d3.line()
+        .x(function (d) {
+            return d.x
+        })
+        .y(function (d) {
+            return ySubScale(d.y)
+        });
+
+    var value = d3.select('#variance').attr('name');
+
+    var data = [{x: 0, y: variance}, {x: INNER_SUB_WIDTH, y: variance}];
+
+    var lineGroup = group.append('g').attr('class', 'lines').classed(value, true);
+    lineGroup.append('path')
+        .attr('d', line(data))
+        .attr('stroke', value)
+        .attr('stroke-width', 2)
+
+}
+
+function d3Variance(numbers) {
+    if (!$('#variance').is(':checked')) {
+        $('#variance').attr('checked', false)
+        d3.select('.sub-graph').remove();
+    }
+    else
+        drawVariance(numbers);
+}
+
+function quantile(numbers) {
+    var quantile = +document.querySelector('.quantile-input input').value;
+    return d3.quantile(numbers, quantile);
+}
+
+
+function d3Scan(numbers) {
+    var retVal = prompt("Enter your name : ", "your name here");
+
+}
 var allMethods = {
     min: d3.min,
     max: d3.max,
@@ -125,13 +210,14 @@ var allMethods = {
     mean: d3.mean,
     median: d3.median,
     quantile: quantile,
-    variance: d3.variance,
-    deviation: d3.deviation
+    variance: d3Variance,
+    deviation: d3.deviation,
 };
 
 function reset() {
     d3.selectAll('.d3ArraysStatisticalMethods svg .lines').remove()
     d3.selectAll('.axis').remove()
+    d3.selectAll('.sub-graph').remove()
 }
 
 function generateCords(selectedNumbers) {
@@ -150,18 +236,19 @@ function myFunction(element) {
     if (!(selectedNumbers instanceof Array))
         selectedNumbers = [selectedNumbers];
     generateCords(selectedNumbers).forEach(function (cords) {
-        if(isChecked)
-            drawLine(cords, color,id);
+        if (isChecked && id !== 'variance')
+            drawLine(cords, color, id);
         else
-            d3.select('.lines .'+id).remove();
+            d3.select('g .' + id).remove();
     })
 }
-createButton();
+createCheckBox();
 setUpSvg();
 createAxis();
 d3UsageArraysStatisticalMethods(numbers)
 function setData() {
     reset();
+    resetCheckBox();
     numbers = getInputNumbers();
     defineDomain(numbers);
     createAxis();
